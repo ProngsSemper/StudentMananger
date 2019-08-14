feb0dec4897adfb4636cb0cd40eb755d90e20eef
胡曦 <798237844@qq.com>
2019-08-14 10:07:56 +0800

    将头像上传方式改为ajax-jQuery，优化上传成功/失败提示，精炼代码

--- src/main/java/student/servlet/UploadServlet.java
+++ src/main/java/student/servlet/UploadServlet.java
@@ -8,23 +8,26 @@ import student.entity.Student;
 import student.service.IStudentService;
 import student.service.impl.StudentServiceImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
+import java.io.PrintWriter;
 import java.util.List;
 
+/**
+ * @author Prongs
+ */
 @WebServlet("/UploadServlet")
 public class UploadServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
         //上传用户头像
         boolean isMultipart = ServletFileUpload.isMultipartContent(request);
         if (isMultipart) {
@@ -32,21 +35,14 @@ public class UploadServlet extends HttpServlet {
             ServletFileUpload upload = new ServletFileUpload(factory);
             try {
                 List<FileItem> items = upload.parseRequest(request);
-                Iterator<FileItem> iterator = items.iterator();
-                while (iterator.hasNext()) {
-                    FileItem item = iterator.next();
+                for (FileItem item : items) {
                     String fakePath = request.getSession().getServletContext().getRealPath("/");
                     String path = fakePath.replace("out\\artifacts\\StundentManager_war_exploded", "src\\main\\webapp\\upload");
                     String fileName = item.getName();
                     String ext = fileName.substring(fileName.indexOf(".") + 1);
                     if (!("png".equals(ext) || "gif".equals(ext) || "jpg".equals(ext))) {
-                        request.setAttribute("error", "uploadError");
                         System.out.println("图片类型有误");
-                        if ("student_login".equals(request.getSession().getAttribute("flag"))) {
-                            request.getRequestDispatcher("student.jsp").forward(request, response);
-                        } else if ("administrator_login".equals(request.getSession().getAttribute("flag"))) {
-                            request.getRequestDispatcher("administrator.jsp").forward(request, response);
-                        }
+                        out.write("uploadError");
                         return;
                     }
                     File file = new File(path, fileName);
@@ -62,9 +58,9 @@ public class UploadServlet extends HttpServlet {
                             String adm = "administrator_login";
                             String flag = "flag";
                             if (adm.equals(request.getSession().getAttribute(flag))) {
-                                response.sendRedirect("QueryStudentByPageServlet");
+                                out.write("adm_success");
                             } else {
-                                response.sendRedirect("QueryStudentByNameServlet");
+                                out.write("stu_success");
                             }
                         }
                         return;
